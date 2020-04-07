a69662cf26e22d6c4de9efca4193c01ecf934030
Atheane <eroyant@gmail.com>
2020-04-07 17:34:23 +0200

    mise en forme pour la page sign up

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SignInForm } from '../components/SignIn'; 
 
 export default () => (
-  <div style={{display: 'flex', justifyContent: 'center', height: '83vh', paddingTop: '200px' }}>
+  <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', paddingTop: '200px' }}>
     <SignInForm />
   </div>
 );
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SignUpForm from '../components/SignUp'; 
 
 export default () => (
-  <div style={{display: 'flex', justifyContent: 'center'}}>
+  <div style={{display: 'flex', justifyContent: 'center', height: '100vh', paddingTop: '200px' }}>
     <SignUpForm />
   </div>
 );
